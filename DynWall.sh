b243c1a8079feb8468f41ac6c11e99a2b27ab06a
albert752 <azemar.albert@gmail.com>
2019-03-14 11:40:14 +0100

    Progress been made

---
+++ DynWall.sh
@@ -0,0 +1,54 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: DynWall.sh
+# 
+#         USAGE: ./DynWall.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 14/03/19 10:19
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+args=("$@")
+HOUR=0
+MIN=0
+
+change_wallpaper(){
+	#if [ $MIN -ge 30 ]
+	#then 
+		#HOUR=$(echo "$HOUR + 0.5" | bc)
+	#fi
+
+	ID=$(echo "($HOUR-1.5)/1.5" | bc)
+	nitrogen --set-zoom-fill ~/Imatges/DynWall/mojave_dynamic_$ID.jpeg
+}
+
+grab_time(){
+	if [ ${args[0]} == "debug" ]
+	then
+		echo "Debugging mode"
+		HOUR=${args[1]}
+		MIN=${args[2]}
+	else
+		HOUR="$(date +"%H")"
+		MIN="$(date +"%M")"
+	fi
+}
+
+while :
+do
+	grab_time
+	change_wallpaper
+	sleep 3s 
+done
+
+
